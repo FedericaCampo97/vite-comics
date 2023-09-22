ad3e5c4901a13c04ea8455412fc84f980a2b3ca9
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-22 17:42:08 +0200

    create components

--- comics/src/App.vue
+++ comics/src/App.vue
@@ -1,17 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import appHeader from './components/AppHeader.vue';
+import appMain from './components/AppMain.vue';
+import appFooter from './components/AppFooter.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <appHeader/>
+
+  <appMain/>
+
+  <appFooter/>
+
 </template>
 
 <style scoped>
