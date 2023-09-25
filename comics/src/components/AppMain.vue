546b889b870476ceeaddc62edb82da523db3fc5c
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-25 15:00:59 +0200

    Add jumbotron

--- comics/src/components/AppMain.vue
+++ comics/src/components/AppMain.vue
@@ -24,12 +24,7 @@ const arrayMain = [
 </script>
 
 <template>
-    <div class="bg-black">
-        <div class="container">
-            <nav class="navbar navbar-expand-lg navbar-light bg-black pt-5 pb-5">
-                <a class="navbar-brand content" href="#"> --&#62; Content goes here &#60;-- </a>
-            </nav>
-        </div>
+    <div class="block_jumbo bg-black">
     </div>
     <div class="block_menu">
         <div class="container">
