3c220f5f382afaae66aae2dc3f7b06bed20f647c
FedericaCampo97 <federica.campo97@gmail.com>
2023-09-26 11:41:24 +0200

    I insert the two buttons

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -40,6 +40,14 @@ export default {
 
 <template>
     <div class="block_jumbo bg-black">
+        <button class="current_button"><strong>CURRENT SERIES</strong></button>
+    </div>
+
+    <div class="card_comics text-center">
+        <div class="container d-flex flex-wrap pt-5 pb-3">
+            <cardElement v-for="element in cards" class="card_element" :thumb="element.thumb" :title="element.series" />
+        </div>
+        <button class="btn btn-primary mt-3 mb-3 custom_button"><strong>LOAD MORE</strong></button>
     </div>
     <div class="block_menu">
         <div class="container">
@@ -52,12 +60,6 @@ export default {
                 </li>
             </ul>
         </div>
-    </div>
-    <div class="card_comics">
-        <div class="container d-flex flex-wrap py-5">
-            <cardElement v-for="element in cards" class="card_element" :thumb="element.thumb" :title="element.series" />
-        </div>
-        <button class="btn btn-primary">LOAD MORE</button>
     </div>
 </template>
 
